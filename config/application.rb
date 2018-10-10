15cceacaabb12ac36f793a629a3a8d30619bb7d9
Ismael Sane <six.echo.sa@gmail.com>
2018-10-10 17:47:52 +0200

    l

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,6 @@ module SylvieReflexo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    # Enable the asset pipeline
-    config.assets.enabled = true
   end
 end
 
