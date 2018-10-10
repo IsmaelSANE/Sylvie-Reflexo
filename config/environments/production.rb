7f23d6e1a157112c6cfb943eaa5c3e11f1131437
Ismael Sane <six.echo.sa@gmail.com>
2018-10-10 15:39:33 +0200

    yaml file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -91,5 +93,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.initialize_on_precompile = false
 end
